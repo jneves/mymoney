16f0bc0b99bb8710caddb2bacc616ad26a0d7f01
Luis Miguel Braga <microft@dsl-2-77.bl26.telepac.pt>
2011-11-11 21:11:28 +0000

    account listing now works for montepio

--- mymoney/montepio.py
+++ mymoney/montepio.py
@@ -12,11 +12,18 @@ import os
 import time
 
 USERLOGINPAGE = "https://net24.montepio.pt/Net24-Web/func/acesso/net24pLoginTV.jsp"
-PINLOGINPAGE = "https://net24.montepio.pt/Net24-Web/func/VLNP/1197592006-1196197144.jsp"
 BASEDOMAIN = "https://net24.montepio.pt"
+MAINPAGE = "https://net24.montepio.pt/Net24-Web/func/homePages/executa.jsp?pagInic=1&selectedNode=100"
+ACCOUNTINDEX ="https://net24.montepio.pt/Net24-Web/func/contasordem/posicaoIntegrada.jsp?selectedNode=101"
+
+class RedirectedException( Exception ):
+    pass
+class AuthenticationException( Exception):
+    pass
 
 class MontepioNet24(Bank):
     def login(self):
+        logging.info("Loging in")
         self.start(self.info["user"], self.info["pass"], "montepio_cookie.txt")
         self.save_session()
 
@@ -30,28 +37,15 @@ class MontepioNet24(Bank):
 
     def authenticate(self, user, password):
         # POST user
-        self.cookiejar= cookielib.LWPCookieJar( )
-        self.opener= urllib2.build_opener( urllib2.HTTPCookieProcessor(self.cookiejar) )
-
         values = {
             'loginid_IN' : user,
             'pageCWS' : 'https://www.montepio.pt/SitePublico/pt_PT/particulares.page',
         }
-        data = urllib.urlencode(values)
-        f = self.opener.open(USERLOGINPAGE, data=data)
-        res1 = f.read()
-
-        #req = urllib2.Request(url, data)
-        #res1 = urllib2.urlopen(req).read()
-
-        #print res1 
-        #print repr( self.cookiejar )
+        res1 = self.get_page( USERLOGINPAGE, parameters=values )
 
         #get the action for the next form
         x = re.findall( r"Net24-Web/func/VLNP/.+?.jsp", res1)
-        #print repr(x)
         url = '/'.join((BASEDOMAIN, x[0]))
-        #print url
 
         # translate password/pin
         pass_translit = {}
@@ -62,23 +56,14 @@ class MontepioNet24(Bank):
             pass_translit[ key ] = value
             new_pass = new_pass.replace(key,value)
 
-        #print repr( [ pass_translit, password, new_pass] )
-
         # POST pin
         values = {
             'pin1_IN' : new_pass,
         }
-        data = urllib.urlencode(values)
-        self.opener.addheaders = (
-                ('User-agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.21 Safari/535.7'),
+        self.opener.addheaders.append(
                 ('Referer','https://net24.montepio.pt/Net24-Web/func/acesso/net24pLoginTV.jsp'),
-                ('Origin', 'https://net24.montepio.pt'),
-            )
-        #print repr( self.opener.addheaders )
-        f = self.opener.open( url, data=data )
-        res2 = f.read()
-        #print repr( f.info().headers )
-        #print res2 
+        )
+        res2 = self.get_page(url, parameters=values, allow_redirects=True )
 
     def load_session(self, file_present=True):
         logging.debug("loading cookie from file")
@@ -86,6 +71,10 @@ class MontepioNet24(Bank):
         #if file_present:
         #    self.cookiejar.load( filename= self.cookie_file, ignore_discard=True)
         self.opener= urllib2.build_opener( urllib2.HTTPCookieProcessor(self.cookiejar) )
+        self.opener.addheaders = [
+                ('User-agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.21 Safari/535.7'),
+                ('Origin', 'https://net24.montepio.pt'),
+            ]
 
     def save_session(self):
         logging.debug("saving cookie to file")
@@ -93,5 +82,37 @@ class MontepioNet24(Bank):
             raise Exception("Cookie filename was not specified on construction")
         self.cookiejar.save( filename= self.cookie_file, ignore_discard=True)
 
+    def get_page(self, url, parameters={}, allow_redirects=False):
+        d= urllib.urlencode(parameters)
+        f= self.opener.open(url, data=d)
+        if not allow_redirects and f.geturl()!=url:
+            raise RedirectedException("got "+f.geturl()+" instead of "+url)
+        html= f.read()
+        #if not get_cxdo_version(html) == CXDO_VERSION:
+            #logging.warn('CXDO site version differs from expected. got %r, expected %r', get_cxdo_version(html), CXDO_VERSION)
+        return html
+
     def is_authenticated(self):
-        return True
+        try:
+            html= self.get_page(MAINPAGE)
+            return True
+        #except RedirectedException:
+        except:
+            return False
+
+    def get_account_list(self):
+        html = self.get_page(ACCOUNTINDEX)
+        #count number of accounts
+        contas = re.findall(r"conta\d\d\d", html)
+        soup = BeautifulSoup(html)
+        contas_info = []
+        for i in range( len(contas) ):
+            conta_id = "%.3d" % i
+            select = soup.find('tr', { 'id' : "c" + conta_id } );
+            conta = []
+            for td in select.findAll( 'td', { 'class' : 'tdClass1'} ):
+                content = re.findall(r">([^<>\n]+?)<", repr(td))
+                conta.append(content[0])
+            contas_info.append( ( conta_id, ' - '.join(conta) ) )
+        return contas_info
+
